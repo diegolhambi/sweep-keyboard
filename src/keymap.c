b7721f5633e8db647f52f177ed028fec3c6fc0a9
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2022-07-11 09:43:01 -0300

    Revert "Flipped arrow keys to make possibly use osm with numbers"
    This reverts commit 5a48967f46242e0536540fc4e4d7744fdc1f62e7.

--- src/keymap.c
+++ src/keymap.c
@@ -10,10 +10,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_EXTENDED] = LAYOUT(
-        KC_ESC,        LALT(KC_LEFT), LGUI(KC_F),    LALT(KC_RIGHT), KC_PSCR,           KC_PGUP, KC_HOME, KC_UP,   KC_END,   KC_CAPS,
-        OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT),  ___,               KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL,
-        LGUI(KC_Z),    LGUI(KC_X),    LGUI(KC_C),    SCMD(KC_K),     LGUI(KC_V),        ___,     ___,     KC_TAB,  ___,      ___,
-                                                                   KC_TRNS, KC_TRNS, KC_TRNS, ___
+        KC_ESC, KC_HOME, KC_UP,   KC_END,   KC_PGUP,        KC_PSCR,    LALT(KC_LEFT), LGUI(KC_F),    LALT(KC_RIGHT), KC_CAPS,
+        KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,        ___,        OSM(MOD_LSFT), OSM(MOD_LGUI), OSM(MOD_LALT),  OSM(MOD_LCTL),
+        ___,    ___,     KC_TAB,  ___,      ___,            LGUI(KC_V), SCMD(KC_K),    LGUI(KC_C),    LGUI(KC_X),     LGUI(KC_Z),
+                                     KC_TRNS, KC_TRNS, KC_TRNS, ___
     ),
 
     [_SYMBOL] = LAYOUT(
